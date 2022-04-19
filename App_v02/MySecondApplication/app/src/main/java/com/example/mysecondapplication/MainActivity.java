4941f7b7607f8181e461aa5746605d358766fdc6
redmoon <bsef19a507@pucit.edu.pk>
2022-04-20 02:29:26 +0500

    App version 0.2

--- App_v02/MySecondApplication/app/src/main/java/com/example/mysecondapplication/MainActivity.java
+++ App_v02/MySecondApplication/app/src/main/java/com/example/mysecondapplication/MainActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void changeText(View view){
         TextView myView = (TextView) findViewById(R.id.textView);
-        myView.setText("لا إله إلا الله محمد رسول الله\n");
+        CharSequence text = "بسم الله الرحمن الرحيم";
+        CharSequence txt = myView.getText();
+        Button btn = (Button) findViewById(R.id.button);
+        if(txt.equals(text)){
+            myView.setText("لا إله إلا الله محمد رسول الله\n");
+            btn.setText("Back to Bismillah");
+        }else{
+            myView.setText("بسم الله الرحمن الرحيم");
+            btn.setText("Read More");
+        }
+
+
+
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # # Mobile_Computing_App_02
-# This is my Second Android App...!
+# This is my Second Android App version 0.1...!
 ![Semantic description of image](/App_v02/screenshot/secondapp.jpg "Image")
+
+# This is my Second Android App version 0.2...!
+![Semantic description of image](/App_v02/screenshot/v2.jpg "Image")
 
